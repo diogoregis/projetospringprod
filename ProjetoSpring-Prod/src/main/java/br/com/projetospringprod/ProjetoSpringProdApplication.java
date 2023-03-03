255369520f81245f12289748b5d1004c2df8ae9c
Diogo Regis <diogo.regis.silva@gmail.com>
2023-03-03 20:50:26 -0300

    Primeiro Commit, start

---
+++ ProjetoSpring-Prod/src/main/java/br/com/projetospringprod/ProjetoSpringProdApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetospringprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringProdApplication.class, args);
+	}
+
+}
---
+++ ProjetoSpring-Prod/src/test/java/br/com/projetospringprod/ProjetoSpringProdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetospringprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
